71259c831d4e8fbb085b396ea8f39e47f5752c44
Thomas Nelis <tomnelis53@gmail.com>
2019-12-03 00:26:43 +0100

    basic backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.URL, (err, database) => {
+    if (err) return console.log(err)
+    // Make sure you add the database name and not the collection name
+    require('./app/routes')(app, database.db("forum"));
+});
+
+app.listen(port, () => {  console.log('We are live on ' + port);});
